d4f8ea7513c2fb2a44446202dec1192cbcca96ee
Juliana Souza <103597775+JulianaR23@users.noreply.github.com>
2022-05-24 07:51:21 -0300

    Update Exercício2.sql

--- Aula02/Exercício2.sql
+++ Aula02/Exercício2.sql
@@ -14,28 +14,28 @@ primary key (id)
 );
 
 insert into db_produtos(nome_produto, valor, categoria, tamanho, quantidade)
-value ("Camisa", 29.99, "Femenino", "M", 20);
+value ("Camisa", 29.99, "Feminino", "M", 20);
 
 insert into db_produtos(nome_produto, valor, categoria, tamanho, quantidade)
 value ("Blusa", 510.99, "Masculino", "P", 10);
 
 insert into db_produtos(nome_produto, valor, categoria, tamanho, quantidade)
-value ("Calça", 79.99, "Femenino", "46", 18);
+value ("Calça", 79.99, "Feminino", "46", 18);
 
 insert into db_produtos(nome_produto, valor, categoria, tamanho, quantidade)
-value ("Camisa", 58.99, "Femenino", "G", 2);
+value ("Camisa", 58.99, "Feminino", "G", 2);
 
 insert into db_produtos(nome_produto, valor, categoria, tamanho, quantidade)
-value ("Vestido", 35.87, "Femenino", "GG", 15);
+value ("Vestido", 35.87, "Feminino", "GG", 15);
 
 insert into db_produtos(nome_produto, valor, categoria, tamanho, quantidade)
 value ("Bermuda", 39.99, "Masculino", "M", 14);
 
 insert into db_produtos(nome_produto, valor, categoria, tamanho, quantidade)
-value ("Camisa", 34.10, "Femenino", "P", 5);
+value ("Camisa", 34.10, "Feminino", "P", 5);
 
 insert into db_produtos(nome_produto, valor, categoria, tamanho, quantidade)
-value ("Vestido", 509.99, "Femenino", "PP", 2);
+value ("Vestido", 509.99, "Feminino", "PP", 2);
 
 select * from db_produtos where valor >= 500;
 
